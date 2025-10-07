025c2042fdc1a02c865a3857a2ca162dc4edb94a
dvdherron <dvdherron@gmail.com>
2025-10-07 12:58:37 +0200

    add dialog demo files

---
+++ src/dialog/script.js
@@ -0,0 +1,15 @@
+const openDialogBtns = document.querySelectorAll(
+  `[data-button*="show-dialog"]`
+);
+const dialog = document.querySelector('dialog');
+const closeDialogBtn = document.getElementById('close');
+
+[...openDialogBtns].forEach((openDialogBtn) => {
+  openDialogBtn.addEventListener('click', () => {
+    dialog.showModal();
+  });
+
+  closeDialogBtn.addEventListener('click', () => {
+    dialog.close();
+  });
+});
